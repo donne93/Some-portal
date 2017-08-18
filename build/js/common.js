d89bfefdaf323b26eb024cda3f5c3091c9598297
donne93 <donne93@mail.ru>
2017-08-18 16:51:56 +0300

    Add files via upload

--- build/js/common.js
+++ build/js/common.js
@@ -1,1 +1,10 @@
 'use strict'
+
+$(document).ready(function() {
+
+    $('.click-to-comment').click(function() {
+        jQuery(document).find('.block-to-write-your-comment').toggle();
+        jQuery(document).find('.click-to-comment').toggle();
+    });
+    
+});
